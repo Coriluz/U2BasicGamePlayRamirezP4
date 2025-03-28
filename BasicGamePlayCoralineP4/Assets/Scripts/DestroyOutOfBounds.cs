334784694ea790a7d06408b927278b084fec33e8
Coriluz <760351@student.dusd.net>
2025-03-28 11:45:58 -0700

    I finished 2.1-.24
    I only need to do the bonus features..

--- BasicGamePlayCoralineP4/Assets/Scripts/DestroyOutOfBounds.cs
+++ BasicGamePlayCoralineP4/Assets/Scripts/DestroyOutOfBounds.cs
@@ -9,7 +9,7 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,9 +17,12 @@ public class DestroyOutOfBounds : MonoBehaviour
     {
         if (transform.position.z > topBound)
         {
+            //If an object goes past the players view in the game, remove that object
             Destroy(gameObject);
-        } else if (transform.position.z < lowerBound)
+        }
+        else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Uh Oh! Game Over!!");
             Destroy(gameObject);
         }
     }
