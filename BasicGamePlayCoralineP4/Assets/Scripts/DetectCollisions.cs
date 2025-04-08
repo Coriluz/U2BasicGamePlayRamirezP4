faaaee28fd3645f3e66102d6e86e14f4076b5194
Coriluz <760351@student.dusd.net>
2025-04-08 11:44:34 -0700

    I finished moving around
    I'm on medium i need to find out how to make the animals leave from the sides.

--- BasicGamePlayCoralineP4/Assets/Scripts/DetectCollisions.cs
+++ BasicGamePlayCoralineP4/Assets/Scripts/DetectCollisions.cs
@@ -17,7 +17,16 @@ public class DetectCollisions : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);
-        Destroy(other.gameObject);
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Uh Oh! Game Over!!");
+            Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
+        
     }
 }
