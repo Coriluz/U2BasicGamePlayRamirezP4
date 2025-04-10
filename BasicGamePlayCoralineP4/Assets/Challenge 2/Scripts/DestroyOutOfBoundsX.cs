7db3726d0c03e8dbd1d2e9826e033161279366e4
Coriluz <760351@student.dusd.net>
2025-04-10 11:12:42 -0700

    I finished a few steps
    Im on the challenge and I got the balls to be randomized.

---
+++ BasicGamePlayCoralineP4/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    public GameObject ground;
+    private float leftLimit = -30;
+    private float bottomLimit = -25;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy dogs if x position less than left limit
+        if (transform.position.x < leftLimit)
+        {
+            Destroy(gameObject);
+        } 
+       void OnTriggerEnter(Collider other) 
+        {
+            if (other.gameObject == ground)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+}
